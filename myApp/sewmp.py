514baf4c55a3c45a4f02781b3cfc5a4eb4c2c1db
Emmanuel <emmanuelmark501@gmail.com>
2023-06-29 09:17:03 -0700

    more template

--- myApp/sewmp.py
+++ myApp/sewmp.py
@@ -3,12 +3,21 @@ from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/home")
 def homepage():
-    return "<h1>Hello, World</h1>"
+    return render_template("index.html", title = "Home")
+
+@app.route("/register")
+def register():
+    return render_template("register.html", title = "Registration")
 
 @app.route("/login")
 def loginpage():
     return render_template("login.html", title = "Login")
+
+@app.route("/about")
+def about():
+    return render_template("about.html", title = "About")
 
 if (__name__) == "__main__":
     app.run(debug = True)
