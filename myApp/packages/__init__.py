dadecd71a6a1477df821ed80aa5915d6f46e8746
Emmanuel <emmanuelmark501@gmail.com>
2023-06-30 15:38:28 +0100

    arranging into packages

---
+++ myApp/packages/__init__.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'My_secret_key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+
+db = SQLAlchemy(app)
+
+from packages import routes
--- myApp/packages/routes.py
+++ myApp/packages/routes.py
@@ -1,16 +1,12 @@
-from flask import Flask, render_template, url_for
-from form import RegistrationForm, LoginForm
-
-
-app = Flask(__name__)
-app.config[ 'SECRET_KEY' ] = '0x7e7'
+from flask import render_template, redirect, url_for, flash
+from packages.form import RegistrationForm, LoginForm
+from packages import app
 
 @app.route("/")
-@app.route("/home")
 def homepage():
     return render_template("index.html", title = "Home")
 
-@app.route("/register")
+@app.route("/register")                     
 def register():
     form = RegistrationForm()
     return render_template("register.html", form = form, title = "Registration")
@@ -18,11 +14,9 @@ def register():
 @app.route("/login", methods = ["GET", "POST"])
 def loginpage():
     form = LoginForm()
-    return render_template("login.html", form = form, title = "Login")
+    return render_template("login.html", form =form, title = "Login")
+
 
 @app.route("/about")
 def about():
     return render_template("about.html", title = "About")
-
-if (__name__) == "__main__":
-    app.run(debug = True)
