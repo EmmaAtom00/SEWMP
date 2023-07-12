56cec96a46eb0e10ca1357e49cf56d837e8ded78
Emmanuel <emmanuelmark501@gmail.com>
2023-07-12 21:14:00 +0100

    settings

--- myApp/packages/__init__.py
+++ myApp/packages/__init__.py
@@ -10,6 +10,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
+login_manager.login_view = 'loginpage'
 
 app.app_context().push()
 
--- myApp/packages/routes.py
+++ myApp/packages/routes.py
@@ -1,8 +1,8 @@
-from flask import render_template, redirect, url_for, flash
+from flask import render_template, redirect, url_for, flash, request
 from packages.form import RegistrationForm, LoginForm
 from packages import app, db, bcrypt
 from packages.models import user, create_task
-from flask_login import login_user, current_user, logout_user
+from flask_login import login_user, current_user, logout_user, login_required
 
 @app.route("/")
 @app.route("/home")
@@ -32,6 +32,8 @@ def register():
 
 @app.route("/login", methods = ["GET", "POST"])
 def loginpage():
+    if current_user.is_authenticated:
+        return redirect(url_for('dashboard'))
     form = LoginForm()
     if form.validate_on_submit():
         password = form.password.data
@@ -41,13 +43,15 @@ def loginpage():
         if User and bcrypt.check_password_hash(User.password, password):
             login_user(User, remember)
             flash(f"Login Successful", 'success')
-            return redirect(url_for('dashboard'))
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('dashboard'))
         else:
             flash(f"Username or Password incorrect", 'danger')
 
     return render_template("login.html", form =form, title = "Login")
 
 @app.route("/dashboard")
+@login_required
 def dashboard():
     return render_template("dashboard.html")
 
@@ -57,6 +61,13 @@ def about():
     return render_template("about.html", title = "About")
 
 @app.route('/logout')
+@login_required
 def logout():
     logout_user()
     return redirect(url_for('homepage'))
+
+@app.route("/setings")
+@login_required
+def settings():
+    image_file = url_for('static', filename = 'images/' + current_user.profile_img)
+    return render_template("settings.html", tiltle = current_user.username + 'settings', image_file = image_file)
